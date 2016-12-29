a08e101f92b8f98d99ea1038f037ead218ae126b
Salviano Ludgério <salviano.lfg@gmail.com>
2016-12-29 12:05:23 -0200

    Add HighScore example

---
+++ HighScore.php
@@ -0,0 +1,19 @@
+<?php
+
+// The problem
+//
+// Get the player with greater score
+$players = [
+  ['name' => 'Steve', 'score' => 6.2],
+  ['name' => 'Bruce', 'score' => 8.2],
+  ['name' => 'Peter', 'score' => 7.2],
+];
+
+$compareScore = function ($p1, $p2){
+	return ($p1['score'] > $p2['score']) ? $p1 : $p2;
+};
+
+//$winningPlayer = array_reduce($players, $compareScore);
+$winningPlayer = $players->reduce($compareScore);
+
+var_dump($winningPlayer);
